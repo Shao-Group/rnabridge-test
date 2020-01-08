ea7d307b70cd885ad454fc2ee72809491586f732
Mingfu Shao <shaomingfu@gmail.com>
2020-01-08 13:37:38 -0500

    update scripts

--- plots/build.figures.sh
+++ plots/build.figures.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-./plot.gtex.sh D156 normal 10 6 9 5 Raw
-./plot.gtex.sh D156 adjust 14 13 17 16 Adjusted
+./summarize.encode10.sh D156
+./summarize.encode65.sh D156
+./summarize.gtex.sh D156
 
 exit
+
+
+./plot.gtex.sh D156 normal 10 6 9 5 Raw
+./plot.gtex.sh D156 adjust 14 13 17 16 Adjusted
 
 ./plot.encode10.sh D156 adjust 14 13 17 16 Adjusted
 ./plot.encode65.sh D156 adjust 14 13 17 16 Adjusted
