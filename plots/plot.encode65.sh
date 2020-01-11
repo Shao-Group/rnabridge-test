734e500301fd5cb47cca9b448892662cd5ee4eb4
Mingfu Shao <shaomingfu@gmail.com>
2020-01-11 14:56:18 -0500

    draw encode65 as two parts

--- plots/plot.encode65.sh
+++ plots/plot.encode65.sh
@@ -12,12 +12,25 @@ for aaa in `echo "stringtie scallop"`
 do
 
 rawdata=$dir/results.$1/encode65-$aaa
+rawdata1=$dir/results.$1/encode65-$aaa-1
+rawdata2=$dir/results.$1/encode65-$aaa-2
 
-prefix=normal-$aaa
-$dir/barplot.sh $outdir $rawdata $prefix 15 11 7 13 9 5 Original
+cat $rawdata | head -n 25 > $rawdata1
+cat $rawdata | tail -n 25 > $rawdata2
 
-prefix=adjust-$aaa
-$dir/barplot.sh $outdir $rawdata $prefix 19 18 17 23 22 21 Adjusted
+prefix=normal-$aaa-1
+$dir/barplot.sh $outdir $rawdata1 $prefix 15 11 7 13 9 5 Original
+
+prefix=adjust-$aaa-1
+$dir/barplot.sh $outdir $rawdata1 $prefix 19 18 17 23 22 21 Adjusted
+
+prefix=normal-$aaa-2
+$dir/barplot.sh $outdir $rawdata2 $prefix 15 11 7 13 9 5 Original
+
+prefix=adjust-$aaa-2
+$dir/barplot.sh $outdir $rawdata2 $prefix 19 18 17 23 22 21 Adjusted
+
+continue
 
 tmpfile=$dir/tmpfile.R
 rm -rf $tmpfile
@@ -28,7 +41,8 @@ rm -rf $tmpfile
 
 done
 
+exit
+
 outputfile=$outdir/summary.encode65
 cat $tmpoutfile | sed 's/.U.*encode65-//g' | sed 's/  */ /g' | sed 's/-/,/g' | sed 's/,0/,WO/g' | sed 's/,A/,WR/g' | sed 's/stringtie/ST/g' | sed 's/star/SR/g' | sed 's/scallop/SC/g' | sed 's/hisat/HI/g'  > $outputfile
-
 $dir/errorbar.sh $outdir $outputfile 0.3
